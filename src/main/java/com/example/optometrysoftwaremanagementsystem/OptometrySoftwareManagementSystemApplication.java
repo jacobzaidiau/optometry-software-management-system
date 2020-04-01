63a2f6f70d037b96299f5e1ac05103ad45031c16
jacobzaidi <jacob@zaidi.id.ua>
2020-04-01 14:07:18 +1000

    Initial code scaffolding.

---
+++ src/main/java/com/example/optometrysoftwaremanagementsystem/OptometrySoftwareManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.optometrysoftwaremanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptometrySoftwareManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptometrySoftwareManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/optometrysoftwaremanagementsystem/OptometrySoftwareManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optometrysoftwaremanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptometrySoftwareManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
